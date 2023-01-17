77982d0fb478ae124afb60a3074cac61bba30614
PedroRiRE <enlosojsdepedro@hotmail.com>
2023-01-17 10:32:30 +0100

    actualizacion1

--- src/product/example/Product.java
+++ src/product/example/Product.java
@@ -2,12 +2,38 @@ package product.example;
 
 public class Product {
 
-    private Integer id;
-    private String manufacturer;
-    private String model;
+    private Integer name;
     private Double price;
-    private Integer 
 
+    public Product() {
+    }
 
+    public Product(Integer name, Double price) {
+        this.name = name;
+        this.price = price;
+    }
 
+    public Integer getName() {
+        return name;
+    }
+
+    public void setName(Integer name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "name=" + name +
+                ", price=" + price +
+                '}';
+    }
 }
